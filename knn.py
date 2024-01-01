7eb34250008b99eaabe076d00ac07708e92f6d45
NurRahmaDiah <149358714+NurRahmaDiah@users.noreply.github.com>
2024-01-01 18:54:12 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib; matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
 import seaborn as sns
 import streamlit as st
